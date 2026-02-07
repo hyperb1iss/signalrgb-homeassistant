edc85a6e623acf584346abb55a455e6e168d558a
cybershoe <adam@genericor.com>
2026-02-07 18:54:21 -0500

    update logger entries for base select class to use entry ID

--- custom_components/signalrgb/select.py
+++ custom_components/signalrgb/select.py
@@ -19,7 +19,6 @@ from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
 )
-from homeassistant.util import slugify
 
 from signalrgb import AsyncSignalRGBClient, SignalRGBException
 from signalrgb.model import Effect, Layout
@@ -165,7 +164,7 @@ async def async_setup_entry(
     LOGGER.info(
         "Adding %s SignalRGB select entities: %s",
         len(entities),
-        [e.entity_id for e in entities],
+        [f"Entry ID: {e._config_entry.entry_id}" for e in entities],
     )
     async_add_entities(entities)
 
@@ -194,9 +193,8 @@ class SignalRGBBaseSelect(CoordinatorEntity, SelectEntity):
             manufacturer=MANUFACTURER,
             model=MODEL,
         )
-        self.entity_id = f"select.signalrgb_{slugify(select_type)}_{slugify(config_entry.entry_id)}"
         LOGGER.debug(
-            "SignalRGB%sSelect initialized: %s", select_type.title(), self.entity_id
+            "SignalRGBSelect %s initialized for entry: %s", select_type.title(), config_entry.entry_id
         )
 
 
